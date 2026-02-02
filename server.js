c1140e3c7e0fbd84d205c571a9c1193a1cd362c5
kingkohlircb0511-bit <kingkohlircb0511@gmail.com>
2026-02-02 16:11:07 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/status", async (req, res) => {
+  const { trainNo, date } = req.query;
+
+  if (!trainNo || !date) {
+    return res.status(400).json({ error: "Missing trainNo or date" });
+  }
+
+  const url = `https://indian-railway-irctc.p.rapidapi.com/api/trains/v1/train/status?trainNo=${trainNo}&date=${date}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "x-rapidapi-host": "indian-railway-irctc.p.rapidapi.com",
+        "x-rapidapi-key": process.env.526b9360f6msh0831a76221eb3e4p1a52e2jsn2f5763853301
+      }
+    });
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (err) {
+    console.error("API error:", err);
+    res.status(500).json({ error: "API failed" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
